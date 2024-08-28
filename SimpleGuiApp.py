5d84cb62e0b81fe944e7e9b018c7f4ecbfac5ce6
tauqeerrumaney <tauqeerrumaney@gmail.com>
2024-08-28 14:09:23 +0200

    Initial commit

--- SimpleGuiApp.py
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/python3
-import tkinter as tk
-import os, sys
-root = tk.Tk() # toplevel
-root.title('DGApp')
-w = tk.Label(root, text='Hello, world!')
-w.pack(fill='both',expand=True,padx=5)
-# root.mainloop() # app is ready
--- hello_new.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import os
-print(os.getenv('HOME'))
-print(os.getenv('USER'))
-print("Hello ",os.getenv('USER'),"!",sep=" ")
-
